1877168e4647ce51ef855862c6fd502d8cb46b32
hyerlo <138830912+HyeeunKim07@users.noreply.github.com>
2024-01-15 16:13:25 +0900

    Create ac_low1.java

---
+++ 김혜은/ac_low1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int A;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+
+        System.out.println(A - 543);
+    }
+
+}
